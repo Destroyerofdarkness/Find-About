5c385dd5a7d3d05f7d0edefc8c9e871d0012c696
Destroyerofdarkness <dawidadogier@gmail.com>
2025-10-22 10:57:11 +0200

    Removed the style.ejs file and copy pasted the content from it into main.css

--- server.js
+++ server.js
@@ -18,7 +18,9 @@ app.use(express.static(path.join(__dirname, "public")))
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
-
+app.get("/", (req,res)=>{
+    res.redirect("/home")
+})
 
 app.use("/home", home)
 
